70f866b4b9d88e521575d03478d6ddab658efda6
suhyun2963 <k40001338@gmail.com>
2020-10-12 10:49:58 +0900

    add init

--- PefamApp/index.js
+++ PefamApp/index.js
@@ -5,7 +5,5 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import Pefam from './Screen/Pefam';
-import Home from './Screen/Home';
 
-AppRegistry.registerComponent(appName, () => Home);
+AppRegistry.registerComponent(appName, () => App);
---
+++ PefamClone/Screen/Pefam.js
@@ -0,0 +1,12 @@
+import React,{Componet} from 'react';
+import {View, Text} from 'react-native';
+
+export default class Pefam extends Componet{
+    render(){
+        return(
+            <View style={{flex:1, backgroundColor:'lightblue'}}>
+
+            </View>
+        );
+    }
+}
